27784d15b8b6596b7bf64b12a7121fa61dec7f18
Yichen Fan <yichen.f1999@outlook.com>
2022-10-20 00:13:37 -0400

    Generate worldline based on chemical potential and time step. TODO: randomly generating starting point for each iteration.

--- worldline.py
+++ worldline.py
@@ -8,14 +8,24 @@ class grid:
     z_size = 10  # spacial dimension for z axis
     N = 2  # number of world lines
     forward = True
+    cx, cy, cz, ct = 0, 0, 0, 0
+    ix, iy, iz, it = 0, 0, 0, 0
+    stop = False
 
     # Use a 100 \times 100 matrix to store all worldlines, 1 for there is a particle and 0 for no particle
     # The 0 axis for time, 1 for x axis, 2 for y axis and 3 for z axis
     location = np.zeros((beta, x_size, y_size, z_size))
 
-    def initial_worldline(self, beta, x, y, z, N):
-        self.location = np.zeros((beta, x, y, z))
+    def __init__(self):
+        self.mu = None
+        self.epsilon = None
+
+    def initial_worldline(self, beta, x, y, z, N, mu, epsilon):
         self.beta = beta
+        self.epsilon = epsilon
+        self.mu = mu
+        t = beta//epsilon
+        self.location = np.zeros((t, x, y, z))
         self.x_size = x
         self.y_size = y
         self.z_size = z
@@ -31,54 +41,67 @@ class grid:
         cn = np.random.randint(0, self.N)
         return ct, cn
 
-    def hop_right(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx + 1, cy, cz]:
-            self.location[ct + 1, cx + 1, cy, cz] = True
+    def hop_right(self):
+        if not self.location[self.ct + 1, self.cx + 1, self.cy, self.cz]:
+            self.location[self.ct + 1, self.cx + 1, self.cy, self.cz] = True
         else:
-            self.location[ct + 1, cx + 1, cy, cz] = False
+            self.location[self.ct + 1, self.cx + 1, self.cy, self.cz] = False
             self.forward = False
+        self.ct += 1
+        self.cx += 1
 
-    def hop_left(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx - 1, cy, cz]:
-            self.location[ct + 1, cx - 1, cy, cz] = True
+    def hop_left(self):
+        if not self.location[self.ct + 1, self.cx - 1, self.cy, self.cz]:
+            self.location[self.ct + 1, self.cx - 1, self.cy, self.cz] = True
         else:
-            self.location[ct + 1, cx - 1, cy, cz] = False
+            self.location[self.ct + 1, self.cx - 1, self.cy, self.cz] = False
             self.forward = False
+        self.ct += 1
+        self.cx -= 1
 
-    def hop_front(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx, cy + 1, cz]:
-            self.location[ct + 1, cx, cy + 1, cz] = True
+    def hop_front(self):
+        if not self.location[self.ct + 1, self.cx, self.cy + 1, self.cz]:
+            self.location[self.ct + 1, self.cx, self.cy + 1, self.cz] = True
         else:
-            self.location[ct + 1, cx, cy + 1, cz] = False
+            self.location[self.ct + 1, self.cx, self.cy + 1, self.cz] = False
             self.forward = False
+        self.ct += 1
+        self.cy += 1
 
-    def hop_back(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx, cy - 1, cz]:
-            self.location[ct + 1, cx, cy - 1, cz] = True
+    def hop_back(self):
+        if not self.location[self.ct + 1, self.cx, self.cy - 1, self.cz]:
+            self.location[self.ct + 1, self.cx, self.cy - 1, self.cz] = True
         else:
-            self.location[ct + 1, cx, cy - 1, cz] = False
+            self.location[self.ct + 1, self.cx, self.cy - 1, self.cz] = False
             self.forward = False
+        self.ct += 1
+        self.cy -= 1
 
-    def hop_up(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx, cy, cz + 1]:
-            self.location[ct + 1, cx, cy, cz + 1] = True
+    def hop_up(self):
+        if not self.location[self.ct + 1, self.cx, self.cy, self.cz + 1]:
+            self.location[self.ct + 1, self.cx, self.cy, self.cz + 1] = True
         else:
-            self.location[ct + 1, cx, cy, cz + 1] = False
+            self.location[self.ct + 1, self.cx, self.cy, self.cz + 1] = False
             self.forward = False
+        self.ct += 1
+        self.cz += 1
 
-    def hop_down(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx, cy, cz - 1]:
-            self.location[ct + 1, cx, cy, cz - 1] = True
+    def hop_down(self):
+        if not self.location[self.ct + 1, self.cx, self.cy, self.cz - 1]:
+            self.location[self.ct + 1, self.cx, self.cy, self.cz - 1] = True
         else:
-            self.location[ct + 1, cx, cy, cz - 1] = False
+            self.location[self.ct + 1, self.cx, self.cy, self.cz - 1] = False
             self.forward = False
+        self.ct += 1
+        self.cz -= 1
 
-    def no_hop(self, cx, cy, cz, ct):
-        if not self.location[ct + 1, cx, cy, cz]:
-            self.location[ct + 1, cx, cy, cz] = True
+    def no_hop(self):
+        if not self.location[self.ct + 1, self.cx, self.cy, self.cz]:
+            self.location[self.ct + 1, self.cx, self.cy, self.cz] = True
         else:
-            self.location[ct + 1, cx, cy, cz] = False
+            self.location[self.ct + 1, self.cx, self.cy, self.cz] = False
             self.forward = False
+        self.ct +=1
 
     def reject(self):
         self.forward = True
@@ -86,23 +109,87 @@ class grid:
     def reverse(self, cx, cy, cz, ct):
         if self.location[ct - 1, cx, cy, cz]:
             self.location[ct - 1, cx, cy, cz] = False
+            self.ct -= 1
         elif self.location[ct - 1, cx + 1, cy, cz]:
             self.location[ct - 1, cx + 1, cy, cz] = False
+            self.ct -= 1
+            self.cx += 1
         elif self.location[ct - 1, cx - 1, cy, cz]:
             self.location[ct - 1, cx - 1, cy, cz] = False
+            self.ct -= 1
+            self.cx -= 1
         elif self.location[ct - 1, cx, cy + 1, cz]:
             self.location[ct - 1, cx, cy + 1, cz] = False
+            self.ct -= 1
+            self.cy += 1
         elif self.location[ct - 1, cx, cy - 1, cz]:
             self.location[ct - 1, cx, cy - 1, cz] = False
+            self.ct -= 1
+            self.cy -= 1
         elif self.location[ct - 1, cx, cy, cz + 1]:
             self.location[ct - 1, cx, cy, cz + 1] = False
+            self.ct -= 1
+            self.cz += 1
         elif self.location[ct - 1, cx, cy, cz - 1]:
             self.location[ct - 1, cx, cy, cz - 1] = False
+            self.ct -= 1
+            self.cz -= 1
+
+    def approve_prob(self):
+        rand = np.random.uniform(0, 1)
+        if rand < self.epsilon:
+            self.hop_up()
+        elif rand < 2 * self.epsilon:
+            self.hop_down()
+        elif rand < 3 * self.epsilon:
+            self.hop_front()
+        elif rand < 4 * self.epsilon:
+            self.hop_back()
+        elif rand < 5 * self.epsilon:
+            self.hop_right()
+        elif rand < 6 * self.epsilon:
+            self.hop_left()
+        else:
+            self.no_hop()
+
+    def reject_prob(self):
+        rand = np.random.uniform(0, 1)
+        if rand < np.exp(- self.mu * self.epsilon):
+            self.reverse(self.cx, self.cy, self.cz, self.ct)
+        else:
+            self.reject()
+            self.approve_prob()
+            if self.ix == self.cx and self.iy == self.cy and self.iz == self.cz and self.it == self.ct:
+                self.stop = True
+
+    def move(self):
+        if self.forward:
+            self.approve_prob()
+            if self.ix == self.cx and self.iy == self.cy and self.iz == self.cz and self.it == self.ct:
+                self.stop = True
+        else:
+            self.reject_prob()
 
     def print_grid(self):
         print(self.location)
 
 
 grid = grid()
-grid.initial_worldline(beta=12, x=10, y=10, z=10, N=2)
+grid.initial_worldline(beta=12, x=10, y=10, z=10, N=2, mu = 5, epsilon=1)
+nstep = 10000
+nsamplestep = 50
+monte = []
+randarr = np.random.uniform(0, 1, nstep)
+
+for i in range(nstep):
+    while not grid.stop:
+        grid.move()
+    if i % nsamplestep == 0:
+        monte.append(grid)
+
+
+
+
+
+
 
